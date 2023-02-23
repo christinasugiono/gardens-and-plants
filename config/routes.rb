61361de81dc9abc53e0bd8c18ec1e2f27c7d430c
Michele Comolli <mikicomolli@gmail.com>
2023-02-23 14:22:29 +0800

    livecode code

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :gardens, only: [:index, :show] do
+    resources :plants, only: [:create]
+  end
 end
